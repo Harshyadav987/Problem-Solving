2aae705dd785e00129e8b9114a0e150581297521
Harsh Yadav <99412065+Harshyadav987@users.noreply.github.com>
2026-08-08 15:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (62.69%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,14 +1,43 @@
+// Approach 1
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//        stringstream SS(s);
+//        string token="";
+//        string result="";
+
+//        while(SS >> token){
+//         result=token+ " "+ result;
+//        }
+//        int n=result.length();
+//        return result.substr(0,n-1);
+//     }
+// };
+
+
+// Approach 2
 class Solution {
 public:
     string reverseWords(string s) {
-       stringstream SS(s);
-       string token="";
-       string result="";
-
-       while(SS >> token){
-        result=token+ " "+ result;
-       }
-       int n=result.length();
-       return result.substr(0,n-1);
+        reverse(s.begin(),s.end());
+    int n=s.length();
+    int l=0,r=0;
+    int i=0;
+    while(i<n){
+        while(i<n && s[i]!=' '){
+            s[r]=s[i];
+            r++;
+            i++;
+        }
+        if(l<r){
+            reverse(s.begin()+l,s.begin()+r);
+            s[r]=' ';
+            r++;
+            l=r;
+        }
+        i++;
+    }
+    s=s.substr(0,r-1);
+    return s;
     }
 };
