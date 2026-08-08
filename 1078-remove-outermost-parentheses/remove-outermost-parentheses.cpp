de6048f96b189ac6e8d27a84b62005106787568c
Harsh Yadav <99412065+Harshyadav987@users.noreply.github.com>
2026-08-08 11:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (23.69%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,26 +1,53 @@
+// #Appproach 1
+
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         stack<char> st;
+//         string ans="";
+//         for(int i=0;i<s.length();i++){
+//             if(st.empty()){
+//                 st.push(s[i]);
+
+//             }
+//         else if(st.size()==1 && s[i]==')'){
+//             st.pop();
+//         }
+//         else if(s[i]==')'){
+//             st.pop();
+//             ans.push_back(s[i]);
+//         }
+//         else{
+//             st.push(s[i]);
+//             ans.push_back(s[i]);
+//         }
+//         }
+//         return ans;
+        
+//     }
+// };
+
+// #Approach 2
+
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char> st;
+        int counter=0;        
         string ans="";
         for(int i=0;i<s.length();i++){
-            if(st.empty()){
-                st.push(s[i]);
-            }
-            else if(s[i]==')'  && st.size()==1){
-                st.pop();
-
-            }
-            else if(s[i]=='('){
-                st.push(s[i]);
-                 ans.push_back(s[i]);
-            }
-            else{
-                
-                st.pop();
-                
-                ans.push_back(s[i]);
-            }
+           if( s[i]=='(' ){
+            if(counter>0){
+            ans.push_back(s[i]);}
+            counter++;
+           }
+           else {
+            counter--;
+                 if(counter>0){
+                ans.push_back(s[i]);}
+           
+           
+        }
         }
         return ans;
         
